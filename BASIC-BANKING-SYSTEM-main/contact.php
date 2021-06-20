450a541aabb491865004d7ee0862f831d7258c12
rohith2422 <67039093+rohith2422@users.noreply.github.com>
2021-06-20 15:29:14 +0530

    Add files via upload

---
+++ BASIC-BANKING-SYSTEM-main/contact.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>BANKING SYSTEM</title>
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+<body>
+    <!-- Nav bar -->
+    <nav class="navbar py-2 navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">IND BANK <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWprqflCC-bWT1VPIn2Pp30t9lAFMzCkjPNXkOeP-l0I7Iw9kZFiMLplln04m-6Z4pUMU&usqp=CAU" alt="Bank" width="50" height="50"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-8 mb-lg-0">
+                    <li class="nav-item py-3">
+                        <a class="nav-link" aria-current="page" href="index.php"><strong>Home</strong></a>
+                    </li>
+                    
+                    <li class="nav-item py-3">
+                        <a class="nav-link" href="https://www.thesparksfoundationsingapore.org/" target="_blank">About</a>
+                    </li>
+                    <li class="nav-item py-3">
+                        <a class="nav-link active" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+   
+        <h2>
+            <style="font-weight: bold;"> <strong><center>LET US KNOW HOW CAN WE HELP YOU!!!</strong> </center>
+        </h2>
+    
+ 
+    <!-- Form -->
+    <div class="container my-1">
+        <h3>Contact Us</h3>
+   
+        <form>
+            <div class="form">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Your message</label>
+              <br>
+              <!-- <input type="password" class="form-control" id="exampleInputPassword1"> -->
+              <textarea name="" id="" cols="70" rows="10"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+    <!-- footer -->
+    <div class="foot">
+        <footer class="bg-light text-center text-lg-start">
+            <!-- Copyright -->
+            <p><center>CONTACT US:987087888<br>
+  <a href="indbank@gmail.com">indbank@gmail.com</a></p></center>
+            <!-- Copyright -->
+        </footer>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+</body>
+</html>
---
+++ BASIC-BANKING-SYSTEM-main/index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>BANKING SYSTEM</title>
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+ <style>
+.button4 {
+  border: none;
+  background-color: #ADD8E6;
+  padding: 20px 38px;
+  font-size: 20px;
+  cursor: pointer;
+  display: inline-block;
+  margin-bottom: 10px;
+}
+
+.button4:{border-radius: 12px;}
+
+.success {color: green;}
+.success:hover {
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+</head>
+<body>
+    <!-- Nav bar -->
+    <nav class="navbar py-2 navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">MI BANK <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWprqflCC-bWT1VPIn2Pp30t9lAFMzCkjPNXkOeP-l0I7Iw9kZFiMLplln04m-6Z4pUMU&usqp=CAU" alt="Bank" width="50" height="50"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-8 mb-lg-0">
+                    <li class="nav-item py-3">
+                        <a class="nav-link active" aria-current="page" href="#"><strong>Home</strong></a>
+                    </li>
+                    
+                    <li class="nav-item py-3">
+                        <a class="nav-link" href="https://www.thesparksfoundationsingapore.org/ " target="_blank">About</a>
+                    </li>
+                    <li class="nav-item py-3">
+                        <a class="nav-link" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        <h2>
+            <strong><marquee> Welcome to MI BANK!</marquee></strong> 
+        </h2>
+    </class>
+   
+<button onclick="window.location.href='money.php';"class="success button4">TRANSFER MONEY</button></a>
+<button onclick="window.location.href='history.php';"class="success button4">TRANSACTION HISTROY</button><br></a></float></button></float>
+     
+    
+                <img src="bank1.jpg" alt="bank" width="1000" height="750" class="d-block w-100" alt="...">
+            </div>
+            
+ 
+      
+            </div>
+        </div>
+    </div>
+    <!-- footer -->
+    <div class="foot">
+        <footer class="bg-light text-center text-lg-start">
+          
+  <p><center>CONTACT US:7659029918<br>
+  <a href="indbank@gmail.com">mibank@gmail.com</a></p></center>
+</footer></div>
+            <!-- Copyright -->
+        
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+</body>
+</html>
